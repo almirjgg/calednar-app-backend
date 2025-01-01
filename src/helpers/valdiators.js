281921854c34bd354d6d88d4dfdf5066f6948a92
almirjgg <almirjgg@gmail.com>
2025-01-01 12:52:06 -0300

    chore: remove validator for loging

--- src/helpers/valdiators.js
+++ src/helpers/valdiators.js
@@ -4,7 +4,6 @@ import { isDate } from './isDate.js';
 const validateLogin = [
   check('email', 'email is required').isEmail(),
   check('password', 'password is required').not().isEmpty(),
-  check('password', 'password must be at least 6 characters').isLength({ min: 6 }),
   validatorFields,
 ];
 
